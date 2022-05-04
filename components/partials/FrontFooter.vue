d3e7b19f8d2fea2804cce2769afbb2c34e0b3dca
poncoe <poncoe4mac@gmail.com>
2022-05-05 02:19:36 +0700

    meonk commit

---
+++ components/partials/FrontFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer id="footer">
+
+    <div class="footer-bottom">
+      <div class="container">
+        <!-- <div class="row">
+          <p class="pull-left">Copyright © 2022 Luckytrue Development.</p>
+          <p class="pull-right">Designed by <span><a target="_blank" href="http://luckytruedev.com">luckytruedev</a></span></p>
+        </div> -->
+      </div>
+    </div>
+
+  </footer>
+</template>
+
+<script>
+    export default {
+        name: "FrontFooter"
+    }
+</script>
+
+<style scoped>
+
+</style>
