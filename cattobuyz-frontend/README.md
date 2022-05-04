2f4a8e3606975ec2945a8d1bf5a1168db75b2ced
poncoe <poncoe4mac@gmail.com>
2022-05-05 02:16:53 +0700

    meonk commit

---
+++ cattobuyz-frontend/README.md
@@ -0,0 +1,35 @@
+# cattobuyz-frontend
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# nuxt.config.js
+1- update the <project directory>/nuxt.config.js
+```
+axios: {
+    baseURL: ''   // lumen project base url
+  }
+  
+  env: {
+      PAYPAL_CLIENT_ID: ""   // paypal client id
+    }
+```
+2- update the <project directory>/admin/nuxt.config.js
+```
+axios: {
+    baseURL: ''    // lumen project base url
+  }
+  
+  env: {
+      PUSHER_APP_KEY: ""   // pusher app key
+    }
+```
+
+## serve with hot reload at localhost:3000 for website and localhost:4000 for admin
+$ npm run dev
+
+## build for production and launch server
+$ npm run build
---
+++ cattobuyz-monolitic/contact-us.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'layout/styles-scripts.php';?>
+</head><!--/head-->
+
+<body>
+     <?php include 'layout/header.php';?>
+	 
+	 <div id="contact-page" class="container">
+    	<div class="bg">
+	    	<div class="row">    		
+	    		<div class="col-sm-12">    			   			
+					<h2 class="title text-center">Contact <strong>Us</strong></h2>
+				</div>			 		
+			</div>    	
+    		<div class="row">  	
+	    		<div class="col-sm-8">
+	    			<div class="contact-form">
+	    				<h2 class="title text-center">Get In Touch</h2>
+	    				<div class="status alert alert-success" style="display: none"></div>
+				    	<form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
+				            <div class="form-group col-md-6">
+				                <input type="text" name="name" class="form-control" required="required" placeholder="Name">
+				            </div>
+				            <div class="form-group col-md-6">
+				                <input type="email" name="email" class="form-control" required="required" placeholder="Email">
+				            </div>
+				            <div class="form-group col-md-12">
+				                <input type="text" name="subject" class="form-control" required="required" placeholder="Subject">
+				            </div>
+				            <div class="form-group col-md-12">
+				                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your Message Here"></textarea>
+				            </div>                        
+				            <div class="form-group col-md-12">
+				                <input type="submit" name="submit" class="btn btn-primary pull-right" value="Submit">
+				            </div>
+				        </form>
+	    			</div>
+	    		</div>
+	    		<div class="col-sm-4">
+	    			<div class="contact-info">
+	    				<h2 class="title text-center">Contact Info</h2>
+	    				<address>
+	    					<p>LuckyTrue Development.</p>
+							<p>Luckytrue Tower, Hackerway no 1</p>
+							<p>Bandung Indonesia</p>
+							<p>Mobile: +62 123 456 789</p>
+							<p>Email: info@luckytruedev.com</p>
+	    				</address>
+	    			</div>
+    			</div>    			
+	    	</div>  
+    	</div>	
+    </div><!--/#contact-page-->
+
+     <?php include 'layout/footer.php';?>
+</body>
+</html>
