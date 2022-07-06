09c73fe1088dbe1c49c4977b73f5f0fb8d751a76
poncoe <poncoe4mac@gmail.com>
2022-07-07 01:39:16 +0700

    product

---
+++ product2-catfood.php
@@ -0,0 +1,15 @@
+
+<?php
+include("koneksi.php");
+$sql = "INSERT INTO cart (img, nameproduct, price) VALUE ('makanankucing.jpeg', 'Cat Food', 20)";
+$query = mysqli_query($db, $sql);
+if($query){
+    ?>
+    <script type="text/javascript">alert('Data Berhasil Ditambahkan!'); document.location.href='index.php';</script>
+    <?php
+}else{
+    ?>
+    <script type="text/javascript">alert('Data Gagal Ditambahkan!'); document.location.href='index.php';</script>
+    <?php
+}
+?>
